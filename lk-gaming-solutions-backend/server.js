be7a4d359e1b3eedc5424f2684c9c5b8579fb769
Lahiru Madhuka <lahirumadhuka4060@gmail.com>
2026-07-23 21:07:47 +0530

    Fixed Bugs

--- lk-gaming-solutions-backend/server.js
+++ lk-gaming-solutions-backend/server.js
@@ -1,27 +1,17 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 
 //middlewares
 app.use(cors());
-app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
 const PORT = process.env.PORT || 8000;
 const HOST = process.env.HOST;
 const MONGOURL = process.env.MONGO_URL;
-
-app.get("/", (req, res) => {
-    res.send("LK Gaming Solutions Server Updated")
-})
-
-app.post("/api/playstation", (req, res) => {
-    console.log(req.body)
-    res.send(req.body)
-})
 
 mongoose
   .connect(MONGOURL)
@@ -34,3 +24,12 @@ mongoose
   .catch((error) => {
     console.log(error);
   });
+
+app.get("/", (req, res) => {
+  res.send("LK Gaming Solutions Server Updated");
+});
+
+app.post("/api/games", (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+});
